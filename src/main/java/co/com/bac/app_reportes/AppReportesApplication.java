75f2c680198e87f971faaa8fb46f33508fbb4202
JORGE <marialuci1019@gmail.com>
2024-12-07 13:31:44 -0500

    primeros datos del proyecto

---
+++ src/main/java/co/com/bac/app_reportes/AppReportesApplication.java
@@ -0,0 +1,13 @@
+package co.com.bac.app_reportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppReportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppReportesApplication.class, args);
+	}
+
+}
